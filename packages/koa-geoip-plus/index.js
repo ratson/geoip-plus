fad551e51c9c6834e9a7298f0416c381e3882141
Ratson <github@ratson.name>
2018-05-04 23:08:07 +0800

    Allow config ctxKey

--- packages/koa-geoip-plus/index.js
+++ packages/koa-geoip-plus/index.js
@@ -4,12 +4,12 @@ const { promisify } = require('util')
 const geoip = require('geoip-plus')
 const ipaddr = require('ipaddr.js')
 
-module.exports = ({ dbPath } = {}) => {
+module.exports = ({ dbPath, ctxKey = 'geoip' } = {}) => {
   const db = geoip.init(dbPath)
   const lookupAsync = promisify(db.lookup.bind(db))
 
   return (ctx, next) => {
-    ctx.geoip = {
+    ctx[ctxKey] = {
       db,
       async lookupByIPv4(ip) {
         const ipv4 = ipaddr.process(ip || ctx.ip).toString()
